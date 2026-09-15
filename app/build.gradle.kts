e252a811af4297d4d0b7cfe5a811a279618e3bbe
rustamreket2008-boop <rustamreket2008@gmail.com>
2026-09-15 19:25:33 +0500

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins { id("com.android.application") }
+android {
+    namespace="tj.rustam.englishtajik"
+    compileSdk=35
+    defaultConfig { applicationId="tj.rustam.englishtajik"; minSdk=23; targetSdk=35; versionCode=1; versionName="1.0" }
+}
