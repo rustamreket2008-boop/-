abc52750dd16e63282d58529fa81670b3d82115e
rustamreket2008-boop <rustamreket2008@gmail.com>
2026-09-25 16:04:20 +0500

    Refactor MainActivity to display text view

--- app/src/main/java/tj/rustam/englishtajik/MainActivity.java
+++ app/src/main/java/tj/rustam/englishtajik/MainActivity.java
@@ -1,18 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.example.myapp">
-    <application
-        android:allowBackup="true"
-        android:icon="@mipmap/ic_launcher"
-        android:label="@string/app_name"
-        android:roundIcon="@mipmap/ic_launcher_round"
-        android:supportsRtl="true"
-        android:theme="@style/Theme.App">
-        <activity android:name=".MainActivity" android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN"/>
-                <category android:name="android.intent.category.LAUNCHER"/>
-            </intent-filter>
-        </activity>
-    </application>
-</manifest>
-  
+package tj.rustam.englishtajik;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("English Tajik App");
+        setContentView(tv);
+    }
+}
