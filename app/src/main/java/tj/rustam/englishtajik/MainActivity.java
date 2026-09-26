ec8bf00511b081e12af033c6ae7d9d83d289bb3f
rustamreket2008-boop <rustamreket2008@gmail.com>
2026-09-26 11:34:37 +0500

    Fix formatting issue in MainActivity.java

--- app/src/main/java/tj/rustam/englishtajik/MainActivity.java
+++ app/src/main/java/tj/rustam/englishtajik/MainActivity.java
@@ -176,4 +176,4 @@ public class MainActivity extends Activity {
         dictionary.put("write", "навиштан");
         dictionary.put("learn", "омӯхтан");
     }
-            }
+                            }
